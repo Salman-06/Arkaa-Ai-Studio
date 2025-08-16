c9a0ea2dee5219fcc41a78e72640699b6e570caa
Salman <salmanprojects16@gmail.com>
2025-08-16 14:35:45 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/sections/portfolio-section.tsx
+++ src/components/sections/portfolio-section.tsx
@@ -17,7 +17,7 @@ const projects = [
   {
     title: 'Serene Garden',
     category: 'Landscape Design',
-    image: 'https://placehold.co/600x400.png',
+    image: 'https://images.unsplash.com/photo-1632161293871-cf2083474e34?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw3fHxTZXJlbmUlMjBHYXJkZW58ZW58MHx8fHwxNzU1MzU0OTE5fDA&ixlib=rb-4.1.0&q=80&w=1080',
     hint: 'zen garden',
   },
   {
